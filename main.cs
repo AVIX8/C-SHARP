086e6e8e26dbf6c50fe837d1dd7fa98b3d16e884
AVIX <avix256@gmail.com>
2020-10-06 16:58:25 +0700

    первый коммит

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace nsp
+{
+    public static class main
+    {
+        public static void Main(string[] args)
+        {
+            Color a = new Color();
+            Console.Write(a.r);
+        }
+
+    }
+}
